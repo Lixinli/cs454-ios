aca4fbf2280c7453bf109d6bdd808e5ec2f0e52c
Lixinli <xinli.ver2@gmail.com>
2016-07-12 10:12:11 -0700

    ColorMaker_incomplete

---
+++ RoshamboWithHistory_incomplete/RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Li, Xinli on 7/12/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var history: [RPSMatch]!
+    
+    // Table View Delegate
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return history.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let CellID = "HistoryCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(CellID, forIndexPath: indexPath)
+        let match = self.history[indexPath.row]
+        cell.textLabel!.text = victoryStatusDescription(match)
+        cell.detailTextLabel!.text = "\(match.p1) vs. \(match.p2)"
+        return cell
+    }
+    
+    func victoryStatusDescription(match: RPSMatch) -> String {
+        
+        if (match.p1 == match.p2) {
+            return "Tie."
+        } else if (match.p1.defeats(match.p2)) {
+            return "Win!"
+        } else {
+            return "Loss."
+        }
+    }
+    @IBAction func dismissHistory(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
