668d66828e7805756a8e83b4559a7b835c755b9a
Lixinli <xinli.ver2@gmail.com>
2016-07-11 20:51:48 -0700

    asd

---
+++ Meme/Meme/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  Meme
+//
+//  Created by Li, Xinli on 7/7/16.
+//  Copyright © 2016 Li, Xinli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct  Meme {
+    var text:String = ""
+    var image:UIImage
+    var memedImage:UIImage
+}
